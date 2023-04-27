9f09abc195773df841853dd407a2555c130880a0
Maria Luiza Pinheiro <102831221+MariaLuiza01@users.noreply.github.com>
2023-04-27 19:55:11 -0300

    Update CartaoFilme.js

--- CartaoFilme.js
+++ CartaoFilme.js
@@ -4,21 +4,48 @@ const baseUrl = 'https://api.otaviolube.com/api/filmes?populate=*'
 
 export default function Cartoes({ filme }){
 
-    const imgUrl = baseUrl + filme.attributes.poster.data.attributes.url
+    const imgUrl = baseUrl + filme.poster.data.attributes.url
 
     return(
         <View style={styles.viewCartao}>
             <Image style={styles.imgFilme} source={{uri: imgUrl}}/>
-            <Text style={styles.Titulo}>Titulo: {filme.attributes.titulo}</Text>
-            <Text style={styles.Sinopse}>Sinopse: {filme.attributes.sinopse}</Text>
+            <Text style={styles.Titulo}>Titulo: {filme.titulo}</Text>
+            <Text style={styles.Sinopse}>Sinopse: {filme.sinopse}</Text>
             <TouchableOpacity style={styles.button}> 
-                <Text style={styles.txtBtnComprar}>Comprar</Text>
+                <Text style={styles.txtBtnComprar}> Comprar </Text>
             </TouchableOpacity>
         </View>
     )
 }
 const styles = StyleSheet.create({
     viewCartao: {
-        color: 'yellow'
+        backgroundColor: '#55B88A',
+        width: '6%',
+        height: '65%',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    imgFilme: {
+        backgroundColor: 'blue',
+        width: '50%',
+        height: '50%',
+        borderRadius: 10
+    },
+    Titulo: {
+        marginTop: 3,
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    Sinopse: {
+        marginTop: 5
+    },
+    txtBtnComprar: {
+        backgroundColor: '#0F4531',
+        borderRadius: 3,
+        padding: 3,
+        marginTop: 20,
+        color: 'white'
     }
 })
