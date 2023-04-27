52db00f529f1b05cf6a4dd8bfc301950aa96f7be
Maria Luiza Pinheiro <102831221+MariaLuiza01@users.noreply.github.com>
2023-04-27 15:00:24 -0300

    Update CartaoFilme.js

--- CartaoFilme.js
+++ CartaoFilme.js
@@ -2,19 +2,23 @@ import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "rea
 
 const baseUrl = 'https://api.otaviolube.com/api/filmes?populate=*'
 
-export default function Cartoes({filme}){
+export default function Cartoes({ filme }){
 
-    const imgUrl = baseUrl + filme.poster.data.attributes.url
+    const imgUrl = baseUrl + filme.attributes.poster.data.attributes.url
 
     return(
         <View style={styles.viewCartao}>
             <Image style={styles.imgFilme} source={{uri: imgUrl}}/>
-            <Text style={styles.Titulo}> filme.attributes.titulo</Text>
-            <Text style={styles.Sinopse}> filme.attributes.sinopse</Text>
+            <Text style={styles.Titulo}>Titulo: {filme.attributes.titulo}</Text>
+            <Text style={styles.Sinopse}>Sinopse: {filme.attributes.sinopse}</Text>
             <TouchableOpacity style={styles.button}> 
                 <Text style={styles.txtBtnComprar}>Comprar</Text>
             </TouchableOpacity>
         </View>
     )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewCartao: {
+        color: 'yellow'
+    }
+})
